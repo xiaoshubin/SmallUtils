11016e60e7b3cff17fa030d84cb906aa504f855e
xiaoshubin <495303648@qq.com>
2017-08-23 17:58:37 +0800

    指纹识别工具类简化

--- src/main/java/com/smallcake/utils/FingerprintUtil.java
+++ src/main/java/com/smallcake/utils/FingerprintUtil.java
@@ -7,107 +7,71 @@ import android.content.Intent;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v4.hardware.fingerprint.FingerprintManagerCompat;
-import android.support.v4.os.CancellationSignal;
-
-
 
 /**
  * Android指纹识别,需要加入权限
  * <uses-permission android:name="android.permission.USE_FINGERPRINT" />
+ * 需要API Level 23，如果达不到不会报错，这些类会什么都不做。
+ * 因为Api中规定了如果回调了Error或者Succeed方法之后，sensor会被关闭
+ * 直到下一次重新调用authenticate方法授权
  */
-@RequiresApi(api = Build.VERSION_CODES.M)
+
 public class FingerprintUtil {
-    private static CancellationSignal cancellationSignal;//清除指纹识别
     public final static int REQUEST_CODE_CONFIRM_DEVICE_CREDENTIALS = 0x008;//连线密码输入正确后回调Code
-    private static KeyguardManager keyguardManager;//连线键盘锁管理器
-
-
-
+    @RequiresApi(api = Build.VERSION_CODES.M)
     public static void callFingerPrint(final Context context, final OnCallBackListenr listener) {
         FingerprintManagerCompat managerCompat = FingerprintManagerCompat.from(context);
         if (!managerCompat.isHardwareDetected()) { //判断设备是否支持
-            if (listener != null) listener.onSupportFailed();
+            listener.onFailed("当前设备不支持指纹解锁！");
             return;
         }
         KeyguardManager keyguardManager = (KeyguardManager) context.getSystemService(context.KEYGUARD_SERVICE);
         if (!keyguardManager.isKeyguardSecure()) {//判断设备是否处于安全保护中
-            if (listener != null) listener.onInsecurity();
+            listener.onFailed("该设备未处于安全保护中！");
             return;
         }
         if (!managerCompat.hasEnrolledFingerprints()) { //判断设备是否已经注册过指纹
-            if (listener != null)listener.onEnrollFailed(); //未注册
+            listener.onFailed("未设置指纹解锁！");
             return;
         }
-        if (listener != null) listener.onAuthenticationStart(); //开始指纹识别
-
-        cancellationSignal = new CancellationSignal(); //必须重新实例化，否则cancel 过一次就不能再使用了
-        managerCompat.authenticate(null, 0, cancellationSignal, new FingerprintManagerCompat.AuthenticationCallback() {
-            @Override//多次尝试后失败，errString是错误信息，如小米的errString：尝试次数过多，请稍后再试。
-            public void onAuthenticationError(int errMsgId, CharSequence errString) {
-                if (listener != null) listener.onAuthenticationError(errMsgId, errString);
-            }
-
-            @Override // 当指纹验证失败的时候会回调此函数，失败之后允许多次尝试，失败次数过多会停止响应一段时间然后再停止sensor的工作
-            public void onAuthenticationFailed() {
-                if (listener != null) listener.onAuthenticationFailed();
-            }
-
-            @Override //指纹验证失败有的时候会有帮助信息：如小米的helpString：手指移动太快，请重试
-            public void onAuthenticationHelp(int helpMsgId, CharSequence helpString) {
-                if (listener != null) listener.onAuthenticationHelp(helpMsgId, helpString);
-
-            }
+        managerCompat.authenticate(null, 0, null, new MyCallBack(listener), null);
+    }
 
 
-            @Override// 当验证的指纹成功时会回调此函数，然后不再监听指纹sensor
-            public void onAuthenticationSucceeded(FingerprintManagerCompat.AuthenticationResult result) {
-
-                if (listener != null) {
-                    listener.onAuthenticationSucceeded(result);
-                }
-                cancel();
-
-            }
-        }, null);
+    public static class MyCallBack extends FingerprintManagerCompat.AuthenticationCallback{
+        OnCallBackListenr listener;
+        public MyCallBack(OnCallBackListenr listener) {
+            this.listener = listener;
+        }
+        @Override//【多次】尝试后失败，errString是错误信息，如小米的errString：【尝试次数过多，请稍后再试。】
+        public void onAuthenticationError(int errMsgId, CharSequence errString) {
+            if (listener != null) listener.onError(errMsgId, errString);
+        }
+        @Override //指纹验证失败有的时候会有帮助信息：如小米的helpString：手指移动太快，请重试
+        public void onAuthenticationHelp(int helpMsgId, CharSequence helpString) {
+            if (listener != null) listener.onFailed(helpString);
+        }
+        @Override // 当指纹验证失败的时候会回调此函数，失败之后允许多次尝试，失败次数过多会停止响应一段时间然后再停止sensor的工作
+        public void onAuthenticationFailed() {
+            if (listener != null) listener.onFailed("指纹验证失败");
+        }
+        @Override// 当验证的指纹成功时会回调此函数，然后不再监听指纹sensor
+        public void onAuthenticationSucceeded(FingerprintManagerCompat.AuthenticationResult result) {
+            if (listener != null) listener.onSucceeded(result);
+        }
     }
 
     public interface OnCallBackListenr {
-
-        void onSupportFailed();//当前设备不支持指纹解锁
-
-        void onInsecurity();//当前设备未处于安全保护中
-
-        void onEnrollFailed();//请到设置中设置指纹,未设置指纹解锁
-
-        void onAuthenticationStart();//开始指纹识别监听
-
-        void onAuthenticationError(int errMsgId, CharSequence errString);//多次尝试失败后异常
-
-        void onAuthenticationFailed();//指纹识别失败回调
-
-        void onAuthenticationHelp(int helpMsgId, CharSequence helpString);//提醒帮助
-
-        void onAuthenticationSucceeded(FingerprintManagerCompat.AuthenticationResult result);//成功
+        void onError(int errMsgId, CharSequence errString);
+        void onFailed(CharSequence errString);
+        void onSucceeded(FingerprintManagerCompat.AuthenticationResult result);
     }
 
     /**
-     * 取消指纹监听
-     * 1.指纹识别成功后，取消
-     * 2.多次识别失败后，进入锁屏密码界面，也需要取消
-     * 否则，指纹识别还在，再次开启指纹识别会立即判定为【多次指纹识别失败】
-     */
-    private static void cancel() {
-        if (cancellationSignal != null) {
-            cancellationSignal.cancel();
-            cancellationSignal = null;
-        }
-    }
-
-    /**
-     * 如果多次指纹解锁失败，调用此方法，跳转到锁屏密码界面
+     * 如果多次指纹解锁失败，调用此方法，跳转到【锁屏密码界面】
      * @param context
      * 如果锁屏秘密正确，需要再对应Activity中加入回调
-     * 注意: 判定条件一定要加&&resultCode==RESULT_OK，否则用户按返回键也会说成功
+     * 注意: 判定条件一定要加 resultCode==RESULT_OK，否则用户按返回键也会说成功
      *
         protected void onActivityResult(int requestCode, int resultCode, Intent data) {
             super.onActivityResult(requestCode, resultCode, data);
@@ -115,9 +79,8 @@ public class FingerprintUtil {
         }
      */
     public static void setKeyguardLockScreenManager(Context context) {
-        keyguardManager = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
+        KeyguardManager keyguardManager = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
         if (keyguardManager != null) {
-
             boolean keyguardSecure = keyguardManager.isKeyguardSecure();//是否拥有锁屏密码
             if (keyguardSecure) {
                 Intent intent = keyguardManager.createConfirmDeviceCredentialIntent("锁屏密码", "输入锁屏密码");
@@ -130,13 +93,6 @@ public class FingerprintUtil {
                 context.startActivity(new Intent("android.settings.SETTINGS").setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
             }
         }
-    }
-
-    /**
-     * 锁屏密码解锁成功，调用清除方法
-     */
-    public static  void clearKeyguardManager() {
-        if (keyguardManager != null) keyguardManager = null;
     }
 
 }
