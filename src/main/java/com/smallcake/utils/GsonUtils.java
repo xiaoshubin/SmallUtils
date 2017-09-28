a1ae6b47237d98dddab1ce749c18b46f3c5dd646
xiaoshubin <495303648@qq.com>
2017-09-28 15:39:44 +0800

    add a strange method

--- src/main/java/com/smallcake/utils/GsonUtils.java
+++ src/main/java/com/smallcake/utils/GsonUtils.java
@@ -4,12 +4,14 @@ package com.smallcake.utils;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * MyApplication --  cq.cake.util
@@ -29,21 +31,21 @@ public class GsonUtils {
         }
     }
 
-    /**
-     * 返回List<Object>
-     * @param list
-     * @param clz
-     * @param <T>
-     * @return
-     */
-    @Deprecated
-    public static <T> List<T> gsonToList(String list, Class<T> clz){
-        List<T> lt = null;
-        if (list!=null&&!list.equals("")){
-            lt = gson.fromJson(list, new TypeToken<List<T>>(){}.getType());
-        }
-        return lt;
-    }
+//    /**
+//     * 返回List<Object>
+//     * @param list
+//     * @param clz
+//     * @param <T>
+//     * @return
+//     */
+//    @Deprecated
+//    public static <T> List<T> gsonToList(String list, Class<T> clz){
+//        List<T> lt = null;
+//        if (list!=null&&!list.equals("")){
+//            lt = gson.fromJson(list, new TypeToken<List<T>>(){}.getType());
+//        }
+//        return lt;
+//    }
     /**
      * 转成list
      * 解决泛型编译期类型被擦除的问题
@@ -104,6 +106,54 @@ public class GsonUtils {
             }.getType());
         }
         return map;
+    }
+
+    /**
+     * 专门针对后台奇葩数据集，明明是数组对象，非要做成map键值对,
+     * 而这里的1，3，8毫无用处，后台说工具生成的，没办法
+     * 先转JsonObject，通过再转List
+     * @param gsonString
+     * @param cls
+     * @param <T>
+     * @return List<T>
+     *
+     * 丢弃掉无用的1，3，8，把后面的value转为对象，再添加到List
+     *
+     *   "catArr": {
+                "1": {
+                "member_cat_id": "11",
+                "member_cat_name": "区县服务中心",
+                "member_cat_amount": "100000.00"
+                },
+                "3": {
+                "member_cat_id": "12",
+                "member_cat_name": "地市级代理",
+                "member_cat_amount": "500000.00"
+                },
+                "8": {
+                "member_cat_id": "13",
+                "member_cat_name": "一级代理",
+                "member_cat_amount": "2000000.00"
+                }
+         }
+
+     *
+     * @注意 目前缺陷，不能强转类型，比如"100000.00"，你在对象中定义此类型为String和double可以，
+     * 你知道这个参数肯定是int类型，不会有小数，但如果你定义为int，就会类型转换错误
+     * 而gson在外面直接转对象是可以定义为int的,在这里由于用到了泛型T，所以行不通
+     *
+     */
+    public static <T> List<T> jsonToMapsToList(String gsonString, Class<T> cls) {
+
+        List<T> list = new ArrayList<T>();
+        JsonObject asJsonObject =  new JsonParser().parse(gsonString).getAsJsonObject();
+        Set<Map.Entry<String, JsonElement>> entries = asJsonObject.entrySet();
+        for (Map.Entry<String, JsonElement> map: entries){
+            T t = gson.fromJson(map.getValue(),cls);
+            list.add(t);
+        }
+
+        return list;
     }
 
 }
